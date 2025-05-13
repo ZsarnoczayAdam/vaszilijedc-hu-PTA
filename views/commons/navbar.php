1259bb3d2f23709a8551ac33e3105c5f4d985af6
ZsarnoczayAdam <zsarno18@gmail.com>
2025-05-13 10:01:20 +0200

    fejlesztett főoldal

--- views/commons/navbar.php
+++ views/commons/navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="index.php">vasilijedc</a>
+    <a class="navbar-brand" href="https://vaszilijedc.hu" target="_blank">vasilijedc</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" 
       aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
